8bf0db27dd3e56c98ae55dc55df8b056d1627e70
Jan Rutger <janrutger@jaruku.nl>
2025-02-13 20:35:52 +0100

    new pong

--- asm/program.asm
+++ asm/program.asm
@@ -6,58 +6,103 @@ call @init_kernel
 
 
 @program
+    . $paddle_sprite 4
+    . $paddle_pointer 1
+    . $paddle_w 1
+    . $paddle_h 1
+    . $paddle_x 1
+    . $paddle_y 1
 
-    . $sprite 9
-    % $sprite 1 1 1 1 0 1 1 1 1
+    % $paddle_sprite 1 1 1 1
+    % $paddle_pointer $paddle_sprite
+    % $paddle_w 1
+    % $paddle_h 4
+    % $paddle_x 3
+    % $paddle_y 14
 
-    . $sprite1 64
-    % $sprite1 0 1 0 1 1 0 1 0 0 1 0 0 0 0 1 0 1 0 0 0 0 0 0 1 1 0 1 0 0 1 0 1 1 0 0 0 0 0 0 1 1 1 0 1 1 0 1 1 0 1 0 0 0 0 1 0 0 0 1 1 1 1 0 0
-    
-    ldi A 8
-    ldi B 8
-    ldi C $sprite1
+# ball pixel
+    . $ball_sprite 1
+    . $ball_pointer 1
+    . $ball_w 1
+    . $ball_h 1
+    . $ball_x 1
+    . $ball_y 1
 
-    ldi X 33
-    ldi Y 16
-
-    ; ldi C \b 
-    ; muli C 20
-    ; ldm M $FONTS
-    ; add C M
-    ; ldi A 4
-    ; ldi B 5
-        
-    :loop
-        int 5
-
-        call @wait
-        int 5
-        ;call @wait
-
-        addi X 1
-        addi Y 1
-    jmp :loop
-
-        
+    % $ball_sprite 1
+    % $ball_pointer $ball_sprite
+    % $ball_w 1
+    % $ball_h 1
+    % $ball_x 60
+    % $ball_y 10
 
 
-:return   
+
+    call @update_paddle
+    :pong
+
+        call @update_ball
+        call @handle_input
+
+    jmp :pong
+
+:end  
+int 2
 halt
 
 
-# helper routines
-@wait
-    ldi L 1000
-    :lus
-        subi L 1
-        nop
-        tst L 0
-        jmpf :lus
+@handle_input 
+    call @KBD_READ
+
+    tst A \Up
+    jmpf :Down
+        # Paddle UP
+        call @update_paddle
+        ldm M $paddle_y
+        subi M 1
+        sto M $paddle_y
+        call @update_paddle
+    ret
+
+    :Down
+    tst A \Down
+    jmpf :return
+        # Paddle down
+        call @update_paddle
+        ldm M $paddle_y
+        addi M 1
+        sto M $paddle_y
+        call @update_paddle
+    ret
+
+    :return
+    tst A \Return
+    jmpf :no_input
+        # exit program
+        jmp :end
+:no_input
 ret
 
+@update_paddle
+    ldm A $paddle_w
+    ldm B $paddle_h
+    ldm C $paddle_pointer
 
+    ldm X $paddle_x
+    ldm Y $paddle_y
 
+    int 5
+ret
 
+@update_ball
+    ldm A $ball_w
+    ldm B $ball_h
+    ldm C $ball_pointer
 
+    
 
+    ldm X $ball_x
+    ldm Y $ball_y
+
+    int 5
+ret
 
