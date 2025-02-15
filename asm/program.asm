504dd4839d5e7e9b697b85df81165681a6f769ed
Jan Rutger <janrutger@jaruku.nl>
2025-02-15 13:17:51 +0100

    some pong game

--- asm/program.asm
+++ asm/program.asm
@@ -6,17 +6,17 @@ call @init_kernel
 
 
 @program
-    . $paddle_sprite 4
+    . $paddle_sprite 8
     . $paddle_pointer 1
     . $paddle_w 1
     . $paddle_h 1
     . $paddle_x 1
     . $paddle_y 1
 
-    % $paddle_sprite 1 1 1 1
+    % $paddle_sprite 1 1 1 1 1 1 1 1 
     % $paddle_pointer $paddle_sprite
     % $paddle_w 1
-    % $paddle_h 4
+    % $paddle_h 2
     % $paddle_x 3
     % $paddle_y 14
 
@@ -55,12 +55,17 @@ call @init_kernel
         tst M 0
             subi M 1
             sto M $ball_update_counter
-        jmpf :no_ball_update     
+        jmpf :no_ball_update   
+
             call @update_ball
-            ldi M 300
+            call @check_collision
+
+            ldi M 200
             sto M $ball_update_counter
         :no_ball_update
+
         call @handle_input
+        
 
     jmp :pong
 
@@ -185,5 +190,46 @@ ret
         add L M 
         sto L $ball_y
             
+ret
+
+
+@check_collision
+
+    ldm K $ball_x
+    ldm M $paddle_x
+
+    tste K M 
+    jmpf :done
+
+    # possible collision 
+    # check the start of the paddle 
+    ldm L $ball_y
+    ldm M $paddle_y
+    tstg M L
+    jmpt :done
+
+    #check the tail of the paddle
+    ldm K $paddle_h
+    add M K 
+    ldi K 32
+    dmod M K 
+    ld M K 
+
+    tstg L M 
+    jmpt :done
+    ;call @draw_ball
+    call @collision
+
+:done
+ret
+
+@collision
+    call @update_paddle
+    ldm M $paddle_h
+    addi M 1
+    sto M $paddle_h
+    ldi M 63
+    sto M $ball_x
+    call @update_paddle
 ret
 
