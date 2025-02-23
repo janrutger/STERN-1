e37e1821833dc2787ba29640a1c01374a68abf29
Jan Rutger <janrutger@jaruku.nl>
2025-02-23 19:26:14 +0100

    char based: tokennicer

--- asm/program.asm
+++ asm/program.asm
@@ -14,10 +14,99 @@ call @init_stern
 @program
     call @get_input_line
 
-    :tokennice_line
-        sto Z $input_buffer_indx
-        
+    call @tokennice_line
+    
 halt
+
+
+@tokennice_line
+    . $token_buffer 64
+    . $token_buffer_pntr 1
+    . $token_buffer_indx 1
+    % $token_buffer_pntr $token_buffer
+    % $token_buffer_indx 0
+
+    sto Z $input_buffer_indx  
+
+    :tokennice
+        # Current token in A 
+        # Next token i B 
+        inc I $input_buffer_indx
+        ldx A $input_buffer_pntr
+        addi I 1
+        ldx B $input_buffer_pntr
+
+        tst A \space
+        jmpt :tokennice
+        tst A \Return
+        jmpt :end_tokens
+
+        call @is_operator
+        nop
+
+    jmp :tokennice
+
+
+    :end_tokens
+        inc I $token_buffer_indx
+        stx Z $token_buffer_pntr
+        inc I $token_buffer_indx
+        stx Z $token_buffer_pntr
+ret
+
+@is_char
+    ldi M \9
+    tstg A M 
+    jmpf :nochar
+
+    ldi M 57
+    tstg M A
+    jmpf :nochar
+
+    ret
+:nochar
+        ret 
+
+@is_operator
+    tst A \-
+    jmpt :isoperator
+    
+    tst A \+
+    jmpt :isoperator
+
+    ret
+:isoperator
+    ret
+
+@is_digit
+    ldi M 19
+    tstg A M 
+    jmpf :nodigit
+
+    ldi M 30
+    tstg M A
+    jmpf :nodigit
+
+    ret
+:nodigit
+        ret 
+
+@is_neg
+    tst A \-
+    jmpf :notneg
+    ldi M 19
+    tstg B M 
+    jmpf :notneg
+
+    ldi M 30
+    tstg M B
+    jmpf :notneg
+
+    ret
+:notneg
+        ret 
+
+
 
 
 
@@ -65,7 +154,7 @@ halt
     :end
         call @print_cursor
         # Store termination in buffer
-        ldi A \null
+        ldi A \Return
         inc I $input_buffer_indx
         stx A $input_buffer_pntr
 ret
