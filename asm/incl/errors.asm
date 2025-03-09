7a92659ed1fd498b3a9f0061a6fcbcb4206b216c
Jan Rutger <janrutger@jaruku.nl>
2025-03-09 19:16:46 +0100

    nicer Fatal error

--- asm/incl/errors.asm
+++ asm/incl/errors.asm
@@ -1,4 +1,4 @@
-@fatal_error
+@fatal_error_old
     ldi X 5
     ldi Y 30
     ld I Y 
@@ -6,4 +6,12 @@
     add I X
     ldi A \f
     stx A $VIDEO_MEM
+halt
+
+
+@fatal_error
+    ldi X 56
+    ldi Y 25
+    ldi C \f 
+    int 3
 halt
