04c5d07878e575d5456698e48933b2fecb89200f
Jan Rutger <janrutger@jaruku.nl>
2025-03-10 19:57:44 +0100

    first stacks commit

--- asm/incl/stacks.asm
+++ asm/incl/stacks.asm
@@ -1,5 +1,16 @@
+. $stacks_line_counter 1
+% $stacks_line_counter 0
+
+. $stacks_program_mem 32
+. $stacks_program_mem_pntr 1
+. $stacks_program_mem_indx 1
+% $stacks_program_mem_pntr $stacks_program_mem
+% $stacks_program_mem_indx 0
+
+
+
 @stacks
-    
+
 
 
 
