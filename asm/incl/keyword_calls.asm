7f9b852a54988942400fc55fa205276f0ae9e982
Jan Rutger <janrutger@jaruku.nl>
2025-03-12 20:13:50 +0100

    Stacks code loop

--- asm/incl/keyword_calls.asm
+++ asm/incl/keyword_calls.asm
@@ -21,7 +21,36 @@ ret
 ret
 
 @begin_kw
-    call @prompt_program
+    # reset mem index (adres)
+    sto Z $stacks_program_mem_indx
+
+    :inst_readline
+        call @prompt_program
+        call @read_stacks_line
+
+        :inst_read
+            call @read_token
+            jmpt :inst_readline
+
+            ldm M $end_hash
+            tste C M 
+            jmpt :begin_kw_end
+
+            # Create label comes here
+            # then inc line counter 
+            inc I $stacks_line_counter
+
+            # keep track of the mem index (adres)
+            inc I $stacks_program_mem_indx
+            stx B $stacks_program_mem_pntr
+
+            inc I $stacks_program_mem_indx
+            stx A $stacks_program_mem_pntr
+            nop
+
+        jmp :inst_read
+        
+:begin_kw_end
 ret
 
 @end_kw
