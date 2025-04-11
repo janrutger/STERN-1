fc7c2b83d3a298f5d53633be6a238c4bbfa04af1
Jan Rutger <janrutger@jaruku.nl>
2025-04-11 13:29:52 +0200

    Start of serial IO sio

---
+++ serialIO.py
@@ -0,0 +1,73 @@
+
+
+class serialIO:
+    def __init__(self, MainMem, BaseAdres) -> None:
+        self.mainmem = MainMem
+
+        self.channels = {}
+        
+        self.channel = BaseAdres
+
+        # commands
+        # 00 (0) open channel
+        # 01 (1) write to channel
+        # 10 (2) read from channel
+        # 11 (3) close channel
+        self.command_register = BaseAdres + 1
+
+        self.data_register = BaseAdres + 2
+
+        # Status
+        # 00 (0) Idle
+        # 01 (1) Data ready
+        # 11 (3) Error
+        self.status_register = BaseAdres + 3
+
+        # set init status to idle
+        self.mainmem.write(self.status_register, 0)
+
+    
+    def access(self) -> None:
+        # read device registers
+        channel = int(self.mainmem.read(self.channel))
+        command = int(self.mainmem.read(self.command_register))
+        data    = int(self.mainmem.read(self.data_register))
+        status  = int(self.mainmem.read(self.status_register))
+
+
+        if status == 1:       # data ready
+            if command == 0:  # open channel
+                self.channels[channel] = []
+                self.mainmem.write(self.status_register, 0)
+
+            elif command == 1: # write to channel
+                if channel in self.channels:
+                    self.channels[channel].append(data)
+                    self.mainmem.write(self.status_register, 0)
+                else:
+                    self.mainmem.write(self.status_register, 3)
+                    
+
+            elif command == 2: # read from channel
+                pass
+
+            elif command == 3: # close channel
+                if channel in self.channels:
+                    del self.channels[channel]
+                    self.mainmem.write(self.status_register, 0)
+                else:
+                    self.mainmem.write(self.status_register, 3)
+            else:
+                pass
+                
+        else:
+            pass
+            
+
+        
+        
+        
+        
+
+        
+        
