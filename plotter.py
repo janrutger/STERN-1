51fae2bb660fa9014a1ac6472ceb2a611392b966
Jan Rutger <janrutger@jaruku.nl>
2025-04-12 21:12:02 +0200

    Start of the plotter

--- plotter.py
+++ plotter.py
@@ -15,22 +15,25 @@ class Plotter:
 
             self.fig, self.ax = plt.subplots()
             plt.ion()
-            plt.show()
-            print("set online..........................")
+            plt.show(block=False)
+            print("set online...........")
 
 
 
         elif not self.sio.check_channel(self.channel) and self.status == 'online':
             self.status = 'offline'
             plt.close('all')
+            print("set offline...........")
 
         if self.status == 'online':
             data = self.sio.read_channel(self.channel)
             if data is not None:
                 self.ax.scatter(self.x_ax, data, s=5, c='black')
                 self.x_ax = self.x_ax + 1
+                plt.show(block=False)
                 plt.draw()
                 plt.pause(0.001)
+                print("write data...........")
                 
                 
 
