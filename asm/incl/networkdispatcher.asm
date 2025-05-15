ba2020c88a664c7d586623f4bdb41d38fa54a1de
Jan Rutger <janrutger@jaruku.nl>
2025-05-15 19:02:29 +0200

    Network Message Dispatcher

--- asm/incl/networkdispatcher.asm
+++ asm/incl/networkdispatcher.asm
@@ -9,6 +9,11 @@
 . $SERVICE0_DATA_BUFFER 16      
 . $SERVICE0_WRITE_PNTR 1        
 . $SERVICE0_DATA_BUFFER_ADRES 1
+% $SERVICE0_WRITE_PNTR 0 
+
+. $SERVICE0_READ_PNTR 1         
+% $SERVICE0_READ_PNTR 0        
+
 % $SERVICE0_DATA_BUFFER_ADRES $SERVICE0_DATA_BUFFER
 
 ;. $temp_src_addr 1    
@@ -17,6 +22,7 @@
 
 @network_message_dispatcher
     call @read_nic_message
+
     ; Expects from @read_nic_message:
     ; A = src_addr, B = data, C = service_id.
     ; If no message, A = \null.
@@ -24,59 +30,78 @@
     tst A \null
     jmpt :nmd_no_message 
 
-    ; Message received. Store details temporarily.
-    ; sto A $temp_src_addr
-    ; sto B $temp_data
-    ; sto C $temp_service_id
-
     ; Validate service_id (C)
     ldm M $MAX_SERVICE_ID
     tstg C M
     jmpt :nmd_invalid_service_id 
-
-    ; C = service_id, which is the index for the jump table.
-    ; Get the handler address from the jump table.
-    ; ldi L $SERVICE_JUMP_TABLE   ; L = base address of jump table
-    ; add L C                     ; L = address of the specific service handler pointer in the table
-    ; ldx K L                     ; K = actual address of the service handler routine
     
     ld I C
     ldx M $SERVICE_JUMP_TABLE
+
     ld I M 
-
-    ; Prepare registers for the service call:
-    ; A = src_addr, B = data, C = service_id
-    ; ldm A $temp_src_addr
-    ; ldm B $temp_data
-    ; ldm C $temp_service_id      ; Pass original service_id to handler
-
-    ; Index in I
     callx $mem_start
+
 :nmd_invalid_service_id
     ; Optional: Handle invalid service ID (e.g., log error, send NACK)
     ; For now, we just call fatal error.
     call @fatal_error
+
 :nmd_no_message
 ret
 
 
 @service_handler_0
-    ; Service 0: Reserved for the "current program"
-    ; Called with A = src_addr, B = data, C = service_id (which is 0)
+; Called with A = src_addr, B = data, C = service_id (which is 0)
     ; Stores only the data (from B) into a ring buffer.
-    ; No buffer full check is performed.
+    ; If the buffer is full, it advances the read pointer, discarding the oldest item,
+    ; to make space for the new item, thus keeping the most recent data in order.
 
+    ; Check if the buffer is full.
+    ; A buffer is full if (write_pointer + 1) % size == read_pointer.
+
+    ldm M $SERVICE0_WRITE_PNTR  
+    ldm L $SERVICE0_READ_PNTR   
+
+    ; K = current write_pointer (copy for calculation)
+    ; K = potential next write_pointer
+    ; K = (potential next write_pointer) % 16
+    ld K M                      
+    addi K 1                    
+    andi K 15                   
+
+    ; Is (next_write_pntr) == read_pntr?
+    ; If true, buffer is full
+    tste K L                    
+    jmpt :s0_buffer_is_full     
+
+:s0_proceed_with_write
+    ; Buffer is not full OR space has been made.
     ; Get current write pointer value into I, and increment it in memory
-    inc I $SERVICE0_WRITE_PNTR 
+    inc I $SERVICE0_WRITE_PNTR
 
     ; Store data (B) using I as the index
     stx B $SERVICE0_DATA_BUFFER_ADRES      
 
-    # Assuming buffer size is 16, Apply modulo operation for wrap-around.
+    ; Finalize the $SERVICE0_WRITE_PNTR update in memory (wrap-around)
     ldm M $SERVICE0_WRITE_PNTR
     andi M 15 
     sto M $SERVICE0_WRITE_PNTR
 ret
+
+:s0_buffer_is_full
+    ; Buffer is full. Advance the read pointer to discard the oldest item.
+    ; inc I $SERVICE0_READ_PNTR will load current read_pntr into I and inc in memory
+    inc I $SERVICE0_READ_PNTR   
+    ldm M $SERVICE0_READ_PNTR   
+    andi M 15               
+    ; Store wrapped new read_pntr    
+    sto M $SERVICE0_READ_PNTR   
+
+    ; Now proceed to write the new data
+    jmp :s0_proceed_with_write  
+
+
+
 
 @service_handler_echo
     ; Service 1: Echo service
@@ -90,7 +115,9 @@ ret
     ; C should be the service_id for the *outgoing* packet.
     ; Let's send the echo back with service_id 0 (targeting "current program" on the sender).
     ldi C 0 
-    call @send_data_packet_sub ; This is the new subroutine in networkR2.asm
+    call @send_data_packet_sub 
+ret
+
 
 
 
@@ -128,4 +155,58 @@ ret
         # check for ACK (status becomes 0)
         tst M 0
     jmpf :wait_for_nic_sending_ack_sub
+ret
+
+
+@read_service0_data
+    ; Reads a byte from the SERVICE0_DATA_BUFFER.
+    ; Returns:
+    ;   In Register A: data byte if available. (Content of A is undefined if buffer is empty and status bit is 0)
+    ;   Status bit: 1 if data was read and is in A.
+    ;               0 if buffer is empty.
+    ; This routine should be called from the main program.
+    ; It is the consumer for data produced by @service_handler_0.
+
+    ; Disable interrupts to ensure atomic access to pointers
+    di 
+
+    ldm M $SERVICE0_READ_PNTR  
+    ldm L $SERVICE0_WRITE_PNTR 
+
+    ; Is read_pointer == write_pointer?
+    tste M L 
+        ; If M == L (empty), tste sets status bit to 1.
+        ; If M != L (data), tste sets status bit to 0.
+
+    ; Jump if status bit is 1 (meaning M == L, buffer is empty)
+    jmpt :s0_buffer_empty_set_status 
+
+    ; Buffer is not empty, data is available
+    ; I = old $SERVICE0_READ_PNTR value, $SERVICE0_READ_PNTR is incremented in memory
+    inc I $SERVICE0_READ_PNTR
+    ldx A $SERVICE0_DATA_BUFFER_ADRES 
+    
+    ; Wrap-around for $SERVICE0_READ_PNTR (which is already incremented in memory)
+    ldm M $SERVICE0_READ_PNTR
+    andi M 15              
+    ; Store the wrapped pointer value back       
+    sto M $SERVICE0_READ_PNTR     
+
+    ; Set status bit to 1 (data available)
+    ldi M 1 
+    ; Sets status bit to 1 because M (1) is not equal to 0.
+    tst M 0 
+    jmp :s0_read_done
+
+:s0_buffer_empty_set_status
+    ; We jumped here because M == L, and tste M L set the status bit to 1.
+    ; We need to set status bit to 0 to indicate buffer empty.
+    ldi M 0 
+    ; Sets status bit to 0 because M (0) is equal to 0.
+    tst M 0 
+    ; Register A's content is not guaranteed here / can be considered garbage.
+
+:s0_read_done
+    ; Enable interrupts
+    ei 
 ret
